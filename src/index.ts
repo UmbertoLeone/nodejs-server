73f9acab04ea4608cabb2e38d76d4a20054c4aee
umberto.leone <umberto.leone04@gmail.com>
2023-09-05 00:52:07 +0200

    feature(router): added router to app main

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import router from "./routes";
 
 const app = express();
 app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    res.json({hello: 'World'});
-})
+app.use(router);
 
 const port = Number(process.env.PORT || 8080);
 app.listen(port, '0.0.0.0', () => {
